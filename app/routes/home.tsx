a8a82a7f4e7be44b4c75a83f7554fac9439ed378
whitewalker-sa <mpholouischauke@outlook.com>
2025-08-24 12:43:40 +0200

    refactor: gradient style

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,5 +8,12 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main></main>;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>;
 }
